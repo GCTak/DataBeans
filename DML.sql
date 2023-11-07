3f3f472fc7b64dd106a0d43f60b6984595d249a6
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-07 00:34:28 -0300

    Adicionando inserção para a procedure DML.sql

--- DML.sql
+++ DML.sql
@@ -1,18 +1,18 @@
---InserÁ„o na Tabela Comanda
+--Inser√ß√£o na Tabela Comanda
 INSERT INTO Comanda (IDCliente, IDItemLanchonete, Quantidade)
 VALUES (1, 1, 2),
 	   (3, 3, 3),
 	   (5, 5, 5);
 
---InserÁ„o Tabela NotaFiscal
+--Inser√ß√£o Tabela NotaFiscal
 INSERT INTO NotaFiscal (DataVenda, IDCliente, ValorTotal, FormaPagamento, IDFuncionario)
-VALUES ('2023-09-13', 1, 100.00, 'Cart„o de CrÈdito', 1),
+VALUES ('2023-09-13', 1, 100.00, 'Cart√£o de Cr√©dito', 1),
 	   ('2023-09-14', 2, 75.50, 'Dinheiro', 1),
-	   ('2023-09-15', 3, 120.25, 'Cart„o de DÈbito', 3),
+	   ('2023-09-15', 3, 120.25, 'Cart√£o de D√©bito', 3),
 	   ('2023-09-16', 4, 50.00, 'Dinheiro', 5),
-	   ('2023-09-17', 5, 80.75, 'Cart„o de CrÈdito',3);
+	   ('2023-09-17', 5, 80.75, 'Cart√£o de Cr√©dito',3);
 
---InserÁ„o na Tabela RegistroDeUso
+--Inser√ß√£o na Tabela RegistroDeUso
 INSERT INTO RegistroDeUso (IDCliente, IDMaquina, DataHoraInicio, DataHoraFim, ValorCobrado)
 VALUES (1, 1, '2023-09-13 08:00:00', '2023-09-13 10:00:00', 50.00),
 	   (2, 2, '2023-09-14 09:30:00', '2023-09-14 11:30:00', 65.75),
@@ -20,13 +20,15 @@ VALUES (1, 1, '2023-09-13 08:00:00', '2023-09-13 10:00:00', 50.00),
 	   (4, 4, '2023-09-16 10:45:00', '2023-09-16 12:45:00', 40.25),
 	   (5, 5, '2023-09-17 13:15:00', '2023-09-17 14:45:00', 55.00);
 
---InserÁ„o de dados na Tabela Reserva
+--Inser√ß√£o de dados na Tabela Reserva
 INSERT INTO Reserva (IDCliente, IDMaquina, Horario)
 VALUES (1, 1, '2023-09-20 10:00:00'),
 	   (3, 3, '2023-09-22 11:15:00'),
 	   (5, 5, '2023-09-24 16:00:00');
 
---InserÁ„o na Tabela Comanda para o uso na funÁ„o
+--Inser√ß√£o na Tabela Comanda para o uso na fun√ß√£o
 INSERT INTO Comanda VALUES (1,5,5);
 
+--Inser√ß√£o na Tabela RegistrodeUso para uso na procedure
+INSERT INTO RegistroDeUso VALUES (4,1,'2023-11-07 00:30', '2023-11-07 02:30', 50)
 
