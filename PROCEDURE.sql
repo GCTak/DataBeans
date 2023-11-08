79708e26ebc7d7e547c4db7b72d2b9098a6367a6
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-08 16:29:52 -0300

    Update PROCEDURE.sql
    Alteração na procedure para adequar às tabelas, já que adicionamos a coluna IDFuncionario nas tabelas Comanda e ItemComanda

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -1,11 +1,11 @@
---Criação Procedure para atualizar o ValorTotal da comanda
+--Criação Procedure para atualizar o ValorTotal da tabela Comanda
 CREATE PROCEDURE AtualizarValorTotalComanda
 AS
 BEGIN
     UPDATE c
     SET ValorTotal = (SELECT SUM(ValorTotalItem) FROM ItemComanda WHERE IDCliente = c.IDCliente)
     FROM Comanda c
-    WHERE c.IDCliente IN (SELECT IDCliente FROM ItemComanda);
+    WHERE c.IDCliente IN (SELECT IDCliente FROM ItemComanda) AND c.IDFuncionario IN (SELECT IDFuncionario From ItemComanda);
 END;
 --Trigger para chamar a procedure automaticamente
 CREATE TRIGGER AtualizarComandaValorTotal
@@ -20,5 +20,5 @@ BEGIN
 END;
 --Teste
 SELECT * FROM Comanda
-INSERT INTO ItemComanda VALUES(1,1,1,'2023-11-07',2.5)
+INSERT INTO ItemComanda VALUES(1,1,1,7,'2023-11-07',2.5)
 SELECT * FROM Comanda
