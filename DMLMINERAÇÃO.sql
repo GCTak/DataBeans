64ea6cb008e7ef98d9a2c7c5acf040768cfc52cc
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-08 20:18:02 -0300

    2º Momento
    Vamos usar esse DML para o 2ºMomento

---
+++ DMLMINERAÇÃO.sql
@@ -0,0 +1,151 @@
+INSERT INTO Cliente VALUES
+	('Ana Silva','1990-05-15','Rua Rio Grande do Sul, 240', '34992781547','ana.silva@hotmail.com'),
+	('Maria do Santos', '1992-03-10','Avenida Pará, 1355', '34995148254', 'maria.santos@gmail.com'),
+	('Roberto Oliveira', '1980-07-25', 'Rua Coronel Antônio Alves Pereira, 400, Condominio Edifício Executivo', '34998762507','eng.roberto@outlook.com'),
+	('Amanda Souza', '1995-02-15', 'Rua Oswaldo Carneiro, 150', '34996650279','amandinhaS2@hotmail.com'),
+	('Carlos Pereira', '1996-11-30', 'Avenida Floriano Peixoto, 557', '34992460113', 'carlao540@gmail.com'),
+	('Juliana Santos', '1990-04-12','Rua Tenente Virmontes, 950', '34997485302', 'jujuba@outlook.com'),
+	('Leandro Cerqueira', '1993-09-05', 'Rua Abdalla Haddad, 197', '34991540013', 'cerqueira17@gmail.com'),
+	('Isabella Fernandes', '1993-06-18', 'Rua Caiapônia, 56', '34933211214','drafernandez@hotmail.com'),
+	('Pedro Oliveira' , '1987-04-30','Rua Agenor Paes, 324,Condominio Edifício Maria Augusta', '34973727181', 'oliveiraman@outlook.com'),
+	('Gabriela Santos', '1999-01-22', 'Avenida João Pinheiro, 618', '34927683131', 'gabrielasan@hotmail.com'),
+	('Sofia Lima', '2002-05-15','Rua Padre Mário Forestan, 55','34926405496','l1m14S0fm4@gmail.com'),
+	('Davi Costa', '2003-09-08', 'Avenida João Pinheiro, 1117','34920085165','costeira@hotmail.com'),
+	('Lara Martins', '2004-02-18','Rua Pr.Cícero Macedo, 63- Edifício Nazira Rassi', '34932680414','larinha18@gmail.com'),
+	('Enzo Rodriguez', '2001-11-30', 'Rua Padre Anchieta, 46 - Residencial Evidence', '34933254445','enzovalentina@hotmail.com'),
+	('Alice Fernandez', '2002-07-23', 'Rua Professor Mário Porto,232','34932481381','alice77783@gmail.com'),
+	('Miguel Almeida','2003-04-10', 'Rua Rodolfo Correa, 427', '34932165186','goodguy101@hotmail.com'),
+	('Arthur Santos', '2008-01-05', 'Avenida Cesário Alvim, 401','34935847874','kingarthur@gmail.com'),
+	('Helena Oliveira', '2010-03-14', 'Rua Duque de Caxias, 495 - Edifício Zenith', '34930736652','Yummilena@hotmail.com'),
+	('Theo Silva', '2013-09-20', 'Rua Rodolfo Correa, 39','34938336165','FenrirPizza@gmail.com'),
+	('Valentina Pereira', '2005-08-22', 'Rua Tobias Inácio, 210', '34931682672','Sakura@hotmail.com');
+
+INSERT INTO Funcionario VALUES 
+	('João Silva','Atendente de Caixa','2022-06-14',1500,'Rua Bernardo Guimarães,66','34927209037','Joao@DataBeans.com'),
+	('Maria Santos','Gerente','2023-01-23', 1700,'Rua Marechal Deodoro,91','34922937689','Maria@DataBeans.com'),
+	('Pedro Oliveira','Técnico de Suporte','2022-09-30', 1650,'Rua General Osório,361','34939576531','Pedro@DataBeans.com'),
+	('Ana Costa','Técnico de Suporte','2022-12-08',1650,'Rua Barão de Camargos, 425','34923282003','Ana@DataBeans.com'),
+	('André Pereira','Barista','2023-03-17',1500,'Avenida Rio Branco,54 - Vilandry Residence','34922735168','Andre@DataBeans.com'),
+	('Laura Fernandes','Barista','2022-08-05',1500,'Rua  Francisco Alves, 116','34925705685','Laura@DataBeans.com'),
+	('Carlos Rodriguez','Barista','2023-04-22',1500,'Rua Lúcia Matos, 101','34924233853','Carlos@DataBeans.com'),
+	('Sofia Almeida','Barista','2022-11-11',1500,'Praça Ladário Teixeira, 27','34931309729','Sofia@DataBeans.com'),
+	('Daniel Souza', 'Barista','2022-07-02',1500,'Rua Vigário Dantas, 491','34935861881','Daniel@DataBeans.com'),
+	('Beatriz Gomez', 'Barista','2023-02-19',1500,'Rua Vicente Guimarães Matos, 30','34930848936','Beatriz@DataBeans.com'),
+	('Sophia Martinez', 'Barista', '2022-09-15',1500,'Rua Engenheiro Diniz,471','34926676435','Sophia@DataBeans.com'),
+	('Tiago Ramos', 'Limpeza e Manutenção','2023-06-26',1100,'Rua Olegário Maciel, 373 - Edifício Place Vendome','34932606215','Tiago@DataBeans.com'),
+	('Clara Mendes', 'Limpeza e Manutenção','2022-10-14',1100,'Rua Gardênia, 64','34920195011','Clara@DataBeans.com');
+
+INSERT INTO Lanchonete VALUES 
+	('Enrolado de salsicha',2.50,15),
+	('Enrolado de presunto e queijo', 2.50,15),
+	('Enrolado de queijo', 2.50, 10),
+	('Bolinha de queijo', 1.8, 15),
+	('Pão de queijo', 2, 10),
+	('Cachorro quente', 4, 10),
+	('X-Salada', 5,12),
+	('Pastel de queijo', 3.5,15),
+	('Pastel de carne', 3.5,15),
+	('Empada de frango', 2, 10),
+	('Lanche natural de frango cremoso', 6.5,10),
+	('Lanche natural de ricota c/ cenoura', 6.5,10),
+	('Mini pizza de calabresa', 2.7, 10),
+	('Mini pizza de portuguesa', 2.7,10),
+	('Mini pizza de quatro queijos', 2.7,10),
+	('Coxinha de frango', 1.8,20),
+	('Risole de presunto e queijo', 1.8,15),
+	('Pudim', 8, 10),
+	('Banoffee', 19.90,10 ),
+	('Brigadeiro',1.80,15),
+	('Picolé',3, 50),
+	('Fandangos Queijo', 1.5,15),
+	('Fandangos - Presunto', 1.5,15),
+	('Cheetos - Requeijão', 2,15),
+	('Doritos', 6.80,15),
+	('Cebolitos', 4.90,15),
+	('Cheetos - Parmesão', 6.75,15),
+	('Coca-Cola- 250ml', 2,50),
+	('Guaraná Antártica - 200ml',1.2,50),
+	('Pepsi - 200ml',1.2,50),
+	('Suco Kapo', 6,20),
+	('Suco Tial',2,15),
+	('Toddynho',1.9,15),
+	('Água Mineral Crystal Sem Gás',4,25),
+	('Energético Monster', 8,20),
+	('Energético Red Bull',17.60,20),
+	('Pirulito Bigbig', 0.35,50),
+	('Chiclete Bubbaloo', 0.35,50),
+	('Pipoca Microondas Yoki', 5, 50);
+
+INSERT INTO Maquina VALUES 
+	('B07YSV1','Reservado', 'Desktop', 'Intel Core i5 8GB 240GB SSD Total',5),
+	('B0BS42M', 'Em manutenção','Desktop', 'Intel Core i7 16GB SSD 480GB',5),
+	('B0C8VLG', 'Em uso', 'Desktop', 'PC Gamer RYZEN 7 5700G, 16GB DDR4 3200MHZ, SSD M.2 NVME 250GB, VEGA 7',6),
+	('B0B1W4G', 'Disponível', 'Desktop', 'PC Gamer OnGaming Core i5 10400F, Galax GeForce RTX 3050 8GB, 16GB Ram, SSD SATA 480GB',6),
+	('B0CL3G2', 'Em manutenção', 'Desktop', 'Computador Intel Core i7 6700, 32gb RAM, SSD 480gb, Fonte 600W, Gabinete com 6 Fans',7),
+	('B09XBMX', 'Disponível', 'Notebook', 'Notebook Acer Aspire 8GB RAM 256GB SSD 15.6" FHD IPS(1920 X 1080) AMD Ryzen 3 500 SERIES AMD RADEON Graphics',7),
+	('B0B8475', 'Reservado', 'Notebook', 'Notebook Lenovo IdeaPad 3i i5 4GB DDR4 RAM LINUX 15.6" (1920 x 1080) 8GB 2666MHz SSD ',8),
+	('SN12345','Em uso', 'Notebook', 'Apple Notebook MacBook Air (de 13 polegadas, Processador M1 da Apple com CPU 8-core e GPU 7-core, 8GB RAM, 256GB)',8),
+	('B09YFP8', 'Em manutenção', 'Notebook', 'ACER Notebook Nitro 5, 8GB 512GB SSD, (NVIDIA RTX 3050), Windows 11, 15.6" LED FHD IPS (1920 x 1080)',9),
+	('B09GQ31', 'Disponível', 'Notebook', 'Notebook Avell - Geforce RTX 3050, i5 11400H , 8GB, SSD NVME 250GB, Windows 10 HSL -15"',9),
+	('B224','Disponível', 'Impressora', 'Impressora Xerox Multifuncional a laser - Monocromática',10),
+	('C7120', 'Disponível','Impressora', 'Impressora a laser Xerox Versalink - Colorida',10);
+
+INSERT INTO Reserva VALUES
+	(5, 1,'2023-11-08 15:00'),
+	(1, 7,'2023-11-10 12:00');
+
+INSERT INTO ItemComanda VALUES 
+	(1,11,7,1,'2023-11-10', 6.5),
+	(1,34,7,1,'2023-11-10', 4),
+	(5,16,5,1, '2023-11-08', 1.8),
+	(5,28,5,1,'2023-11-08', 2),
+	(5,20,5,1,'2023-11-08',1.8),
+	(13,23,11,1,'2023-11-03',1.5),
+	(13,29,11,1,'2023-11-03',1.2),
+	(17,27,11,1,'2023-10-30',6.75),
+	(17,29,11,1,'2023-10-30',1.2),
+	(17,21,11,2,'2023-10-30',6),
+	(20,25,11,1,'2023-10-20',6.8),
+	(20,28,11,1,'2023-10-20',2),
+	(20,37,11,3,'2023-10-20',1.05),
+	(9,13,6,1,'2023-11-06',2.7),
+	(9,35,6,1,'2023-11-06',8),
+	(9,25,6,1,'2023-11-06',6.8),
+	(9,35,6,1,'2023-11-06', 8),
+	(9,37,6,2,'2023-11-06', 0.7),
+	(4,36,6,1,'2023-11-06',17.6),
+	(4,2,6,1,'2023-11-06',2.5),
+	(4,23,6,1,'2023-11-06',1.5),
+	(4,36,6,1,'2023-11-06',17.6);
+
+INSERT INTO Manutencao VALUES
+	(2,'2023-11-02','Troca do SSD', 0),
+	(5,'2023-10-30', 'Formatação',0),
+	(9,'2023-11-05', 'Troca Memória RAM',0);
+
+INSERT INTO ServicoAdicional VALUES
+	('Cópia Preto-Branco', 0.5,11),
+	('Cópia Colorida', 1,12),
+	('Impressão A4 Preto-Branco',0.5,11), 
+	('Impressão A4 Colorida',1, 12), 
+	('Digitalização documentos',2, 11),
+	('Digitalização documentos',2, 12),
+	('Corujão', 5, 1),
+	('Corujão', 5, 2),
+	('Corujão', 5, 3),
+	('Corujão', 5, 4),
+	('Corujão', 5, 5);
+
+INSERT INTO RegistroDeUso VALUES 
+	(9,3,'2023-11-06 00:00', '2023-11-07 05:00', 25), 
+	(4,4,'2023-11-06 00:00', '2023-11-07 05:00', 25);
+
+INSERT INTO Comanda VALUES 
+	(1,7,'2023-11-10', 10.50),
+	(4,6,'2023-11-06',39.20),
+	(5,5,'2023-11-08',5.60),
+	(9,6,'2023-11-06',26.20),
+	(13,11,'2023-11-03',2.70),
+	(17,11,'2023-10-30',13.95),
+	(20,11,'2023-10-20',9.85)
+
