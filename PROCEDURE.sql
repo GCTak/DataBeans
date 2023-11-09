79b2eaea84b2190c2b9cd27420e73d7f78cee121
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-09 17:46:25 -0300

    Update PROCEDURE.sql
    Adequação da procedure e da Trigger

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -6,12 +6,12 @@ AS
 BEGIN
     UPDATE c
     SET ValorTotal = (
-            SELECT SUM(ic.ValorTotalItem)
-            FROM ItemComanda ic
-            WHERE ic.IDCliente = @IDCliente AND ic.DataVenda = @DataVenda
-        )
+        SELECT ISNULL(SUM(ic.ValorTotalItem), 0)
+        FROM ItemComanda ic
+        WHERE ic.IDCliente = @IDCliente AND CONVERT(DATE, ic.DataVenda) = @DataVenda
+    )
     FROM Comanda c
-    WHERE c.IDCliente = @IDCliente AND c.IDFuncionario IN (SELECT IDFuncionario FROM ItemComanda WHERE IDCliente = @IDCliente AND DataVenda = @DataVenda);
+    WHERE c.IDCliente = @IDCliente AND CONVERT(DATE, c.DataVenda) = @DataVenda;
 END;
 
 -- Trigger para chamar a procedure automaticamente
@@ -21,7 +21,18 @@ AFTER INSERT
 AS
 BEGIN
     DECLARE @IDCliente INT, @DataVenda DATE;
-    SELECT @IDCliente = IDCliente, @DataVenda = DataVenda
-    FROM inserted;
-    EXEC AtualizarValorTotalComanda @IDCliente, @DataVenda;
+    SELECT TOP 1 @IDCliente = IDCliente, @DataVenda = CONVERT(DATE, DataVenda)
+    FROM inserted
+    ORDER BY DataVenda DESC;
+    IF @IDCliente IS NOT NULL AND @DataVenda IS NOT NULL
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM Comanda c
+            WHERE c.IDCliente = @IDCliente AND CONVERT(DATE, c.DataVenda) = @DataVenda
+        )
+        BEGIN
+            EXEC AtualizarValorTotalComanda @IDCliente, @DataVenda;
+        END;
+    END;
 END;
