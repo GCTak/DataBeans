b63ebf023c29ee2442e0cebfffe66c20a41b92c0
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-10 01:16:54 -0300

    2º Momento
    Correção do erro

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -27,8 +27,9 @@ BEGIN
     IF @IDCliente IS NOT NULL AND @DataVenda IS NOT NULL
     BEGIN
         IF EXISTS (
-            SELECT 1
-            FROM Comanda c
+			SELECT 1 
+			FROM Comanda  c
+			INNER JOIN Pessoa p ON c.IDFuncionario = p.ID
             WHERE c.IDCliente = @IDCliente AND CONVERT(DATE, c.DataVenda) = @DataVenda
         )
         BEGIN
@@ -36,3 +37,16 @@ BEGIN
         END;
     END;
 END;
+
+--Necessário executar esse script antes para contar com os que já foram inseridos, depois daqui executa como esperado
+INSERT INTO ItemComanda VALUES (1,37,27,1,'2023-11-07',0.35);
+INSERT INTO ItemComanda VALUES (1,37,27,1,'2023-11-10',0.35); 
+INSERT INTO ItemComanda VALUES (4,37,26,1,'2023-11-06',0.35);
+INSERT INTO ItemComanda VALUES(5,37,25,1,'2023-11-08',0.35);
+INSERT INTO ItemComanda VALUES (9,37,26,1,'2023-11-06',0.35);
+INSERT INTO ItemComanda VALUES(13,37,31,1,'2023-11-03',0.35); 
+INSERT INTO ItemComanda VALUES(17,37,31,1,'2023-10-30',0.35);
+INSERT INTO ItemComanda VALUES(20,37,31,1,'2023-10-20',0.35); 
+INSERT INTO ItemComanda VALUES(8,37,21,1,'2023-11-15',0.35);
+INSERT INTO ItemComanda VALUES(3,37,22,1,'2023-11-20',0.35);
+
