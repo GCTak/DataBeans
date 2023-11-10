8ab5778508586416068f068fbb554745e6fb63b2
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-09 23:07:28 -0300

    2º Momento
    Refazendo a TRANSACTION

--- TRANSACTION.sql
+++ TRANSACTION.sql
@@ -1,9 +1,12 @@
---Transação  - Commit
 BEGIN TRANSACTION;
-	INSERT INTO Pessoa VALUES('Lucas Eduardo', '2002-11-15', 'Rua Cleantro Vieira, 466', '34999999999', 'luquinhas420@hotmail.com');
-COMMIT;
-
---Transação - Rollback
-BEGIN TRANSACTION;
-	INSERT INTO Reserva VALUES (6, 1, '2023-11-11 18:00','01:00:00');
-ROLLBACK
+	IF EXISTS(SELECT * FROM Maquina WHERE Estado = 'Disponível')
+	BEGIN
+		INSERT INTO Reserva VALUES(10,9,'2023-11-15 16:00','05:00')
+	COMMIT
+	PRINT 'Reserva efetuada com sucesso!';
+   END
+   ELSE 
+	BEGIN
+		ROLLBACK
+		PRINT 'Máquina não está disponível para reserva';
+END;
