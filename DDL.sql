458669f33c226ad5484d6eff8c52211708d4cb93
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-13 21:19:15 -0300

    Update DDL.sql
    Adicionando uma nova coluna na tabela NotaFiscal

--- DDL.sql
+++ DDL.sql
@@ -3,7 +3,7 @@ CREATE DATABASE DataBeans;
 
 --Criação das tabelas
 CREATE TABLE Maquina (
-    IDMaquina INT IDENTITY(1,1) PRIMARY KEY,
+    IDMaquina INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
     NumeroSerie CHAR(7),
     Estado VARCHAR(30),
     Tipo VARCHAR(30),
@@ -13,17 +13,17 @@ CREATE TABLE Maquina (
 );
 
 CREATE TABLE Reserva (
-    IDReserva INT IDENTITY(1,1) PRIMARY KEY,
+    IDReserva INT IDENTITY(1,1)NOT NULL PRIMARY KEY ,
     IDCliente INT NOT NULL,
-    IDMaquina INT NULL,
+    IDMaquina INT NOT NULL,
     Horario DATETIME2 NOT NULL,
-	TempoDeReserva TIME NOT NULL,
+    TempoDeReserva TIME NOT NULL,
     FOREIGN KEY (IDCliente) REFERENCES Cliente(IDCliente),
     FOREIGN KEY (IDMaquina) REFERENCES Maquina(IDMaquina)
 );
 
 CREATE TABLE RegistroDeUso (
-    IDRegistroUso INT IDENTITY(1,1) PRIMARY KEY,
+    IDRegistroUso INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     IDCliente INT NOT NULL,
     IDMaquina INT NOT NULL,
     DataHoraInicio DATETIME2 NOT NULL,
@@ -37,20 +37,20 @@ CREATE TABLE Lanchonete (
     IDItemLanchonete INT IDENTITY(1,1) PRIMARY KEY,
     Nome VARCHAR(100) NOT NULL,
     Preco DECIMAL(10, 2) NOT NULL,
-    Quantidade INT
+    Quantidade INT NOT NULL
 );
 
 CREATE TABLE Manutencao (
-    IDManutencao INT IDENTITY(1,1) PRIMARY KEY,
+    IDManutencao INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     IDMaquina INT NOT NULL,
     DataManutencao DATE NOT NULL,
     Descricao VARCHAR(100),
-    Estado BIT,
+    Estado BIT NOT NULL,
     FOREIGN KEY (IDMaquina) REFERENCES Maquina(IDMaquina)
 );
 
 CREATE TABLE ServicoAdicional (
-    IDServicoAdicional INT IDENTITY(1,1) PRIMARY KEY,
+    IDServicoAdicional INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
     Nome VARCHAR(150) NOT NULL,
     Preco DECIMAL(10, 2) NOT NULL,
     IDMaquina INT NOT NULL,
@@ -101,9 +101,11 @@ CREATE TABLE NotaFiscal (
     IDServicoAdicional INT,
     ValorTotalServicoAdicional DECIMAL(10,2),
     IDComanda INT,
+    QuantidadeDeItens INT,
     ValorTotalComanda DECIMAL(10,2),
     IDFuncionario INT NOT NULL,
-    Desconto DECIMAL(5, 2) NULL,
+    Desconto DECIMAL(5, 2),
+    TributosTotaisIncidentes DECIMAL(10,2),
     FormaPagamento VARCHAR(50),
     ValorTotal DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (IDCliente) REFERENCES Cliente(IDCliente),
