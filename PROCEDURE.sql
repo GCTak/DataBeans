f4950d303842946e186ce1556e3dea545d14f5f1
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-14 14:55:28 -0300

    2º Momento
    Criamos uma nova procedure

--- PROCEDURE.sql
+++ PROCEDURE.sql
@@ -1,39 +1,22 @@
--- Criação Procedure para atualizar o ValorTotal da tabela Comanda
-CREATE PROCEDURE AtualizarValorTotalComanda
-    @IDCliente INT,
-    @DataVenda DATE
+CREATE PROCEDURE DeletarCliente
 AS
 BEGIN
-    UPDATE c
-    SET ValorTotal = (
-        SELECT ISNULL(SUM(ic.ValorTotalItem), 0)
-        FROM ItemComanda ic
-        WHERE ic.IDCliente = @IDCliente AND CONVERT(DATE, ic.DataVenda) = @DataVenda
-    )
-    FROM Comanda c
-    WHERE c.IDCliente = @IDCliente AND CONVERT(DATE, c.DataVenda) = @DataVenda;
-END;
-
--- Trigger para chamar a procedure automaticamente
-CREATE TRIGGER AtualizarComandaValorTotal
-ON ItemComanda
-AFTER INSERT
-AS
-BEGIN
-    DECLARE @IDCliente INT, @DataVenda DATE;
-    SELECT TOP 1 @IDCliente = IDCliente, @DataVenda = CONVERT(DATE, DataVenda)
-    FROM inserted
-    ORDER BY DataVenda DESC;
-    IF @IDCliente IS NOT NULL AND @DataVenda IS NOT NULL
-    BEGIN
-        IF EXISTS (
-			SELECT 1 
-			FROM Comanda  c
-			INNER JOIN Pessoa p ON c.IDFuncionario = p.ID
-            WHERE c.IDCliente = @IDCliente AND CONVERT(DATE, c.DataVenda) = @DataVenda
-        )
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        IF EXISTS (SELECT 1 FROM Cliente WHERE StatusConta = 'Inativa')
         BEGIN
-            EXEC AtualizarValorTotalComanda @IDCliente, @DataVenda;
-        END;
-    END;
+            DELETE FROM Cliente 
+            WHERE StatusConta = 'Inativa';
+            PRINT 'Clientes inativos excluídos com sucesso.';
+        END
+        ELSE
+        BEGIN
+            PRINT 'Não há clientes inativos para excluir.';
+        END
+        COMMIT; 
+    END TRY
+    BEGIN CATCH
+        ROLLBACK; 
+        PRINT 'Erro ao excluir clientes inativos. Transação desfeita.';
+    END CATCH
 END;
