5274957cf50b2bd83c07569b98b6b10504b3dbc5
GCT <120527112+GCTak@users.noreply.github.com>
2023-11-14 00:18:02 -0300

    DML2MOMENTO.sql
    Inserção na tabela NotaFiscal

--- DML2MOMENTO.sql
+++ DML2MOMENTO.sql
@@ -265,3 +265,31 @@ INSERT INTO DetalheServicoAdicional VALUES (15,17,1,5,'2023-11-09');
 INSERT INTO DetalheServicoAdicional VALUES (16,18,1,5,'2023-11-09');
 UPDATE Manutencao SET Estado = 1;
 UPDATE Maquina SET Estado = 'Disponível' WHERE Estado = 'Reservado';
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-10-20', 20, 7, 5, 9.85, 31, 0.99, 'Pix', 10.84);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-10-30', 17, 6, 4, 13.95, 31, 1.40, 'Cartão de Débito', 15.35);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,Troco,ValorTotal)
+VALUES ('2023-11-03', 13, 5, 2, 2.70,31,0.27, 'Dinheiro',2.03, 2.97);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-06', 4, 2,'05:00:00',25, 10,5,2,4,39.20,26,6.92,'Cartão de Crédito',76.12);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-06',9,1,'05:00:00',25, 9,5,4,6,26.20,26,5.62,'Cartão de Crédito',61.82);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,Troco, ValorTotal)
+VALUES ('2023-11-08',5,3,3,5.60,25,0.56,'Dinheiro',3.84,6.16);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-09',11,5,'07:00:00',35,14,5,10,6,26.40,25,6.64,'Pix',73.04);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,ValorTotal)
+VALUES ('2023-11-09',12,6,'07:00:00',35,15,5,11,6,26.40,26,6.64,'Cartão de Crédito',73.04);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-09',14,7,'07:00:00',35,16,5,12,6,26.90,44,6.69,'Cartão de Débito',73.59);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,Troco, ValorTotal)
+VALUES ('2023-11-09',15,8,'07:00:00',35,17,5,13,6,32.50,45,7.25,'Dinheiro', 20.25,79.75);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDRegistroUso, TempoUso, ValorUso, IDServicoAdicional, ValorTotalServicoAdicional, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-09',16,5,'07:00:00', 35, 18,5,14,6,32.45,47,7.25,'Cartão de Crédito',79.70);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,Troco, ValorTotal)
+VALUES ('2023-11-10',1,1,2,10.15,27,1.05,'Dinheiro', 0.05,11.55);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento, ValorTotal)
+VALUES ('2023-11-15',8,8,2,13.00,21,1.3,'Pix',14.3);
+INSERT INTO NotaFiscal (DataVenda, IDCliente, IDComanda, QuantidadeDeItens, ValorTotalComanda, IDFuncionario, TributosTotaisIncidentes, FormaPagamento,Troco, ValorTotal)
+VALUES ('2023-11-20', 3,9,1,8.00,22,0.8,'Dinheiro',1.2,8.8)
